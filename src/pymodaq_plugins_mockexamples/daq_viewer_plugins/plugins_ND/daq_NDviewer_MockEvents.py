25fafec94974bc3e9c0e9dd4aa9fd18c51b2efe4
Sébastien Weber <seba.weber@gmail.com>
2023-10-31 17:29:00 +0100

    created h5 photon file and working (slow) histogrammer plugin

--- src/pymodaq_plugins_mockexamples/daq_viewer_plugins/plugins_ND/daq_NDviewer_MockEvents.py
+++ src/pymodaq_plugins_mockexamples/daq_viewer_plugins/plugins_ND/daq_NDviewer_MockEvents.py
@@ -1,10 +1,22 @@
+from pathlib import Path
+import queue
+import time
+import tempfile
+from threading import Lock
+
+import numpy as np
+from qtpy import QtCore
+
 from pymodaq.utils.daq_utils import ThreadCommand
-from pymodaq.utils.data import DataFromPlugins, Axis, DataToExport
+from pymodaq.utils.data import DataFromPlugins, Axis, DataToExport, DataRaw, DataCalculated
 from pymodaq.control_modules.viewer_utility_classes import DAQ_Viewer_base, comon_parameters, main
 from pymodaq.utils.parameter import Parameter
+from pymodaq.utils.parameter.utils import iter_children
+from pymodaq_plugins_mockexamples.hardware.photon_yielder import PhotonYielder, Photon
+from pymodaq.utils.h5modules.saving import H5Saver
+from pymodaq.utils.h5modules.data_saving import DataEnlargeableSaver, DataToExportEnlargeableSaver, DataLoader
 
-
-
+lock = Lock()
 
 class DAQ_NDViewer_MockEvents(DAQ_Viewer_base):
     """ Instrument plugin class for a 2D viewer.
@@ -12,37 +24,44 @@ class DAQ_NDViewer_MockEvents(DAQ_Viewer_base):
     This object inherits all functionalities to communicate with PyMoDAQ’s DAQ_Viewer module through inheritance via
     DAQ_Viewer_base. It makes a bridge between the DAQ_Viewer module and the Python wrapper of a particular instrument.
 
-    TODO Complete the docstring of your plugin with:
-        * The set of instruments that should be compatible with this instrument plugin.
-        * With which instrument it has actually been tested.
-        * The version of PyMoDAQ during the test.
-        * The version of the operating system.
-        * Installation instructions: what manufacturer’s drivers should be installed to make it run?
-
     Attributes:
     -----------
-    controller: object
+    controller: PhotonYielder
         The particular object that allow the communication with the hardware, in general a python wrapper around the
          hardware library.
-         
-    # TODO add your particular attributes here if any
-
     """
+    grabber_start_signal = QtCore.Signal(float, int)
+    saver_start_signal = QtCore.Signal()
+
     params = comon_parameters + [
-        ## TODO for your custom plugin
-        # elements to be added here as dicts in order to control your custom stage
-        ############
+        {'title': 'Acqu. Time (s)', 'name': 'acqui_time', 'type': 'float', 'value': 10},
+        {'title': 'Refresh Time (ms)', 'name': 'refresh_time', 'type': 'int', 'value': 500},
+        {'title': 'Wait Time (ms)', 'name': 'wait_time', 'type': 'int', 'value': 1},
+        {'title': 'Histogram:', 'name': 'histogram', 'type': 'group', 'children': [
+            {'title': 'Apply weight?', 'name': 'apply_weight', 'type': 'bool', 'value': False},
+            {'title': 'Time min (µs)', 'name': 'time_min', 'type': 'float', 'value': 0},
+            {'title': 'Time max (µs)', 'name': 'time_max', 'type': 'float', 'value': 20},
+        ]},
     ]
 
     def ini_attributes(self):
-        #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
-        #  autocompletion
-        self.controller: PythonWrapperOfYourInstrument = None
+        self.controller: PhotonYielder = None
 
-        # TODO declare here attributes you want/need to init with a default value
+        self.x_axis: Axis = None
+        self.y_axis: Axis = None
+        self.time_axis: Axis = None
 
-        self.x_axis = None
-        self.y_axis = None
+        self.h5temp : H5Saver() = None
+        self.temp_path : tempfile.TemporaryDirectory = None
+        self.saver: DataToExportEnlargeableSaver = None
+        self._loader: DataLoader = None
+
+        self.saver_thread: QtCore.QThread = None
+
+        self._queue = queue.Queue()
+
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.process_events)
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
@@ -53,9 +72,8 @@ class DAQ_NDViewer_MockEvents(DAQ_Viewer_base):
             A given parameter (within detector_settings) whose value has been changed by the user
         """
         # TODO for your custom plugin
-        if param.name() == "a_parameter_you've_added_in_self.params":
-            self.controller.your_method_to_apply_this_param_change()
-        #elif ...
+        if param.name() in iter_children(self.settings.child('histogram'), []):
+            self.process_events()
 
     def ini_detector(self, controller=None):
         """Detector communication initialization
@@ -72,34 +90,51 @@ class DAQ_NDViewer_MockEvents(DAQ_Viewer_base):
         initialized: bool
             False if initialization failed otherwise True
         """
-        raise NotImplemented  # TODO when writing your own plugin remove this line and modify the one below
+
         self.ini_detector_init(old_controller=controller,
-                               new_controller=PythonWrapperOfYourInstrument())
+                               new_controller=PhotonYielder())
 
-        ## TODO for your custom plugin
-        # get the x_axis (you may want to to this also in the commit settings if x_axis may have changed
-        data_x_axis = self.controller.your_method_to_get_the_x_axis()  # if possible
-        self.x_axis = Axis(data=data_x_axis, label='', units='', index=1)
+        callback = PhotonCallback(self.controller, self._queue)
+        self.callback_thread = QtCore.QThread()  # creation of a Qt5 thread
+        callback.moveToThread(self.callback_thread)  # callback object will live within this thread
+        callback.data_sig.connect(self.emit_data)
 
-        # get the y_axis (you may want to to this also in the commit settings if y_axis may have changed
-        data_y_axis = self.controller.your_method_to_get_the_y_axis()  # if possible
-        self.y_axis = Axis(data=data_y_axis, label='', units='', index=0)
+        self.grabber_start_signal.connect(callback.grab)
+        self.callback_thread.callback = callback
 
-        ## TODO for your custom plugin. Initialize viewers pannel with the future type of data
-        self.dte_signal_temp.emit(DataToExport('myplugin',
-                                               data=[DataFromPlugins(name='Mock1', data=["2D numpy array"],
-                                                                     dim='Data2D', labels=['dat0'],
-                                                                     axes=[self.x_axis, self.y_axis]), ]))
+        self.callback_thread.start()
+
+        self.dte_signal_temp.emit(DataToExport('Events', data=[DataCalculated('TOF', data=[np.array([0, 1, 2])],
+                                                                              )
+                                                               ]
+                                               ))
 
         info = "Whatever info you want to log"
         initialized = True
         return info, initialized
 
+    def emit_data(self):
+        self.timer.stop()
+        print('acquisition is done')
+
+
+    def process_events(self):
+        lock.acquire()
+        dwa = self._loader.load_data('/RawData/myphotons/Data0D/CH00/EnlData00', load_all=True)
+        lock.release()
+        time_of_flight, time_array = np.histogram(dwa.axes[0].get_data(), bins=256,
+                                                  range=(self.settings['histogram', 'time_min'] * 1e-6,
+                                                         self.settings['histogram', 'time_max'] * 1e-6),
+                                                  weights=dwa.data[0] if self.settings['histogram', 'apply_weight']
+                                                  else None)
+        print(f'Nphotons: {dwa.size}')
+        dwa_tof = DataCalculated('TOF', data=[time_of_flight],
+                                 axes=[Axis('Time', 's', time_array[:-1])])
+        self.dte_signal_temp.emit(DataToExport('TOF', data=[dwa_tof]))
+
     def close(self):
         """Terminate the communication protocol"""
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
+        self.saver.close()
 
     def grab_data(self, Naverage=1, **kwargs):
         """Start a grab from the detector
@@ -112,36 +147,86 @@ class DAQ_NDViewer_MockEvents(DAQ_Viewer_base):
         kwargs: dict
             others optionals arguments
         """
-        ## TODO for your custom plugin: you should choose EITHER the synchrone or the asynchrone version following
+        if self.h5temp is not None:
+            self.h5temp.close()
+            self.temp_path.cleanup()
+        if self.saver_thread is not None:
+            if self.saver_thread.isRunning():
+                self.saver_thread.terminate()
+                while self.saver_thread.isRunning():
+                    QtCore.QThread.msleep(100)
+                    print('Thread still running')
 
-        ##synchrone version (blocking function)
-        data_tot = self.controller.your_method_to_start_a_grab_snap()
-        self.dte_signal.emit(DataToExport('myplugin',
-                                          data=[DataFromPlugins(name='Mock1', data=data_tot,
-                                                                dim='Data2D', labels=['label1'],
-                                                                x_axis=self.x_axis,
-                                                                y_axis=self.y_axis), ]))
+        self.h5temp = H5Saver(save_type='detector')
+        self.temp_path = tempfile.TemporaryDirectory(prefix='pymo')
+        addhoc_file_path = Path(self.temp_path.name).joinpath('temp_data.h5')
+        self.h5temp.init_file(custom_naming=True, addhoc_file_path=addhoc_file_path)
+        self.h5temp.get_set_group('/RawData', 'myphotons')
+        self.saver: DataToExportEnlargeableSaver = DataToExportEnlargeableSaver(self.h5temp,
+                                                                                axis_name='photon index',
+                                                                                axis_units='index')
+        self._loader = DataLoader(self.h5temp)
 
-        ##asynchrone version (non-blocking function with callback)
-        self.controller.your_method_to_start_a_grab_snap(self.callback)
-        #########################################################
+        self.controller.ind_grabed = -1
 
-    def callback(self):
-        """optional asynchrone method called when the detector has finished its acquisition of data"""
-        data_tot = self.controller.your_method_to_get_data_from_buffer()
-        self.dte_signal.emit(DataToExport('myplugin',
-                                          data=[DataFromPlugins(name='Mock1', data=data_tot,
-                                                                dim='Data2D', labels=['label1'],
-                                                                x_axis=self.x_axis,
-                                                                y_axis=self.y_axis), ]))
+        save_callback = SaverCallback(self._queue, self.saver)
+        self.saver_thread = QtCore.QThread()
+        save_callback.moveToThread(self.saver_thread)
+        self.saver_thread.callback = save_callback
+        self.saver_start_signal.connect(save_callback.work)
+        self.saver_thread.start()
+
+        self.grabber_start_signal.emit(self.settings['acqui_time'], self.settings['wait_time'])
+        self.saver_start_signal.emit()
+        self.timer.setInterval(self.settings['refresh_time'])
+        self.timer.start()
+
     def stop(self):
         """Stop the current grab hardware wise if necessary"""
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_stop_acquisition()  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
-        ##############################
         return ''
+
+
+class PhotonCallback(QtCore.QObject):
+    """
+
+    """
+    data_sig = QtCore.Signal()
+
+    def __init__(self, photon_grabber: PhotonYielder, event_queue: queue.Queue):
+        super().__init__()
+        self.photon_grabber = photon_grabber
+        self.event_queue = event_queue
+
+    def grab(self, acquisition_time: float, wait_time: int):
+        start_acqui = time.perf_counter()
+        while (time.perf_counter() - start_acqui) <= acquisition_time:
+            photon: Photon = self.photon_grabber.grab()
+            self.event_queue.put(photon)
+            QtCore.QThread.msleep(wait_time)
+        self.data_sig.emit()
+
+
+class SaverCallback(QtCore.QObject):
+    def __init__(self, event_queue: queue.Queue, saver: DataToExportEnlargeableSaver):
+        super().__init__()
+        self.event_queue = event_queue
+        self.saver = saver
+
+    def work(self):
+        while True:
+            photon: Photon = self.event_queue.get()
+            data = DataToExport('photons', data=[
+                DataRaw('time', data=[np.array([photon.intensity]),
+                                      np.array([photon.x_pos]),
+                                      np.array([photon.y_pos]),
+                                      ],
+                        labels=['intensity', 'x_pos', 'y_pos']
+                        )
+            ])
+            lock.acquire()
+            self.saver.add_data('/RawData/myphotons', axis_value=photon.time_stamp, data=data)
+            lock.release()
+            self.event_queue.task_done()
 
 
 if __name__ == '__main__':
--- src/pymodaq_plugins_mockexamples/hardware/photon_yielder.py
+++ src/pymodaq_plugins_mockexamples/hardware/photon_yielder.py
@@ -4,23 +4,32 @@ Created the 30/10/2023
 
 @author: Sebastien Weber
 """
+from pathlib import Path
+
+import numpy as np
 
 
 import numpy as np
-data_memory_map = np.load('../resources/KXe_000203_raw.npy', mmap_mode='r')
+
+here = Path(__file__).parent.parent
+data_memory_map = np.load(here.joinpath('resources/KXe_000203_raw.npy'), mmap_mode='r')
 
 
 class Photon:
 
     def __init__(self, photon_array: np.ndarray):
         self.index = int(photon_array[0])
-        self.time_stamp = int(photon_array[1])
+        self.time_stamp = float(photon_array[4])
         self.x_pos = int(photon_array[2])
         self.y_pos = int(photon_array[3])
-        self.intensity = int(photon_array[4])
+        self.intensity = int(photon_array[5])
+
+    def to_positions_intensity(self):
+        return np.array([self.x_pos, self.y_pos, self.intensity])
 
     def __repr__(self):
-        return f'Photon event {self.index}: x:{self.x_pos}, y: {self.y_pos}, time: {self.time_stamp}'
+        return f'Photon event {self.index}: x:{self.x_pos}, y: {self.y_pos},' \
+               f' time: {self.time_stamp}, intensity: {self.intensity}'
 
 
 class PhotonYielder:
@@ -39,7 +48,31 @@ class PhotonYielder:
 
 
 if __name__ == '__main__':
+    from pathlib import Path
+    from qtpy import QtCore
+    from pymodaq.utils.h5modules.saving import H5SaverLowLevel
+    from pymodaq.utils.daq_utils import ThreadCommand
+    from pymodaq.utils.data import DataFromPlugins, Axis, DataToExport, DataRaw, DataCalculated, DataWithAxes
+    from pymodaq.utils.h5modules.data_saving import DataEnlargeableSaver, DataToExportEnlargeableSaver, DataLoader, DataSaverLoader
+
+    here = Path(r'C:\Users\weber\Labo\Programmes Python\PyMoDAQ_Git\pymodaq_plugins_folder\pymodaq_plugins_mockexamples\src\pymodaq_plugins_mockexamples\resources')
+
+    addhoc_file_path = here.joinpath('photons.h5')
+    h5temp = H5SaverLowLevel()
+    h5temp.init_file(file_name=addhoc_file_path, new_file=True)
+    h5temp.get_set_group('/RawData', 'myphotons')
+
+    saver = DataSaverLoader(h5temp)
+
+    data = DataWithAxes('timepix', source='raw', distribution='spread', nav_indexes=(0,),
+                        data=[data_memory_map[:, 5], data_memory_map[:, 2], data_memory_map[:, 3]],
+                        axes=[Axis('tof', 's', data=data_memory_map[:, 4])])
+    saver.add_data('/RawData/myphotons', data, save_axes=True)
+    h5temp.close_file()
     photon = PhotonYielder()
     ind = 0
     for ind in range(100):
         print(photon.grab())
+
+
+    pass
