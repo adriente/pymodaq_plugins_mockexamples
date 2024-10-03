f1780dcb1ed5175dd36f704c651ee4ee9f2ecf35
Sébastien Weber <seba.weber@gmail.com>
2024-10-03 21:36:17 +0200

    proper import

--- hatch_build.py
+++ hatch_build.py
@@ -1,8 +1,9 @@
 from pathlib import Path
-
+from pymodaq_utils.logger import set_logger, get_module_name
 from pymodaq_utils.resources.hatch_build_plugins import PluginInfoTomlHook
 
 here = Path(__file__).absolute().parent
+logger = set_logger(get_module_name(__file__))
 
 
 class PluginInfoTomlHook(PluginInfoTomlHook):
--- src/pymodaq_plugins_mockexamples/__init__.py
+++ src/pymodaq_plugins_mockexamples/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from pymodaq.utils.logger import set_logger  # to be imported by other modules.
+from pymodaq_utils.logger import set_logger  # to be imported by other modules.
 try:
     from _version import __version__, __version_tuple__, version_tuple, version
 except ModuleNotFoundError:
