dc90cfde387010561555d5bbf6a49e65b6b63aa0
Sébastien Weber <seba.weber@gmail.com>
2024-10-03 22:44:27 +0200

    proper build

--- hatch_build.py
+++ hatch_build.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 from pymodaq_utils.logger import set_logger, get_module_name
-from pymodaq_utils.resources.hatch_build_plugins import PluginInfoTomlHook
+from hatchling.metadata.plugin.interface import MetadataHookInterface
+
+from pymodaq_utils.resources.hatch_build_plugins import update_metadata_from_toml
 
 here = Path(__file__).absolute().parent
 logger = set_logger(get_module_name(__file__))
 
 
-class PluginInfoTomlHook(PluginInfoTomlHook):
+class PluginInfoTomlHook(MetadataHookInterface):
     def update(self, metadata: dict) -> None:
-        super().update_custom(metadata, here)
+        update_metadata_from_toml(metadata, here)
+
--- src/pymodaq_plugins_mockexamples/__init__.py
+++ src/pymodaq_plugins_mockexamples/__init__.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 from pymodaq_utils.logger import set_logger  # to be imported by other modules.
-try:
-    from _version import __version__, __version_tuple__, version_tuple, version
-except ModuleNotFoundError:
-    pass
+
+from pymodaq_utils.utils import get_version, PackageNotFoundError
 from .utils import Config
+
+
 config = Config()
+try:
+    __version__ = get_version(__package__)
+except PackageNotFoundError:
+    __version__ = '0.0.0dev'
+
 
